c10dd29953c306fb0f7f980e85619227d2b6be40
Piotr Olber <polber.dev@gmail.com>
2023-05-17 14:45:14 +0200

    Initial commit

---
+++ src/navigators/BottomNavigator.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const BottomNavigator = () => {
+    return (
+        <View>
+            <Text>BottomNavigator</Text>
+        </View>
+    );
+};
+
+export default BottomNavigator;
---
+++ src/screens/NewsFeedScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const NewsFeedScreen = () => {
+    return (
+        <View>
+            <Text>NewsFeedScreen</Text>
+        </View>
+    );
+};
+
+export default NewsFeedScreen;
